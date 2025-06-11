89b289182fd58836f4f8aa4c6b13b7ee3225103d
Jolareka <joaquinroldand42@gmail.com>
2025-06-11 16:12:56 -0300

    Correccion bdd

--- Truway/db/olimpiadas.sql
+++ Truway/db/olimpiadas.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Base de datos: `olimpiadas`
 --
 
+CREATE DATABASE IF NOT EXISTS `olimpiadas` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `olimpiadas`;
+
 -- --------------------------------------------------------
 
 --
