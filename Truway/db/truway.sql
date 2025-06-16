c7946ca30bd44b279557d3068914cfeccb81a4f7
Jolareka <joaquinroldand42@gmail.com>
2025-06-16 18:07:41 -0300

    Update truway.sql

--- Truway/db/truway.sql
+++ Truway/db/truway.sql
@@ -1,42 +1,26 @@
-
 CREATE DATABASE IF NOT EXISTS `truway` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `truway`;
 
-
 CREATE TABLE `estado_facturacion` (
   `id_estado` int(11) NOT NULL,
-  `estado` enum('pago','pendiente') NOT NULL DEFAULT 'pendiente',
   PRIMARY KEY (`id_estado`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `apellido` varchar(100) NOT NULL,
-  `email` text NOT NULL,
-  `contrasena` varchar(50) NOT NULL,
-  `fecha_nacimiento` date NOT NULL,
-  `telefono` text NOT NULL,
-  `rol` enum('admin','cliente') NOT NULL DEFAULT 'cliente',
   PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `productos` (
   `id_producto` int(11) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `descripcion` varchar(150) DEFAULT NULL,
-  `precio` float(11,2) DEFAULT NULL,
-  `tipo_producto` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `tipo_producto` (
   `id_tipo` int(11) NOT NULL,
-  `tipo` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_tipo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- 2. tablas que dependen de productos o usuarios
 CREATE TABLE `carrito` (
   `id_carrito` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
@@ -54,10 +38,6 @@ CREATE TABLE `paquetes` (
 CREATE TABLE `estadias` (
   `id_estadia` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `localidad` varchar(70) DEFAULT NULL,
-  `nombre_hotel` varchar(70) DEFAULT NULL,
-  `servicios` varchar(150) DEFAULT NULL,
-  `categoria` enum('1','2','3','4','5') DEFAULT NULL,
   PRIMARY KEY (`id_estadia`),
   KEY `id_producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -65,10 +45,6 @@ CREATE TABLE `estadias` (
 CREATE TABLE `excursiones` (
   `id_excursion` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `ubicacion_salida` varchar(70) DEFAULT NULL,
-  `duracion` int(11) NOT NULL,
-  `guia` tinyint(1) DEFAULT NULL,
-  `dificultad` enum('alta','media','baja') DEFAULT NULL,
   PRIMARY KEY (`id_excursion`),
   KEY `id_producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -76,10 +52,6 @@ CREATE TABLE `excursiones` (
 CREATE TABLE `pasajes` (
   `id_pasaje` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `origen` varchar(70) DEFAULT NULL,
-  `destino` varchar(70) DEFAULT NULL,
-  `aerolinea` varchar(70) DEFAULT NULL,
-  `tipo_pasaje` enum('solo_ida','ida_y_vuelta') DEFAULT NULL,
   PRIMARY KEY (`id_pasaje`),
   KEY `id_producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -87,23 +59,14 @@ CREATE TABLE `pasajes` (
 CREATE TABLE `vehiculos` (
   `id_vehiculo` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `marca` varchar(70) DEFAULT NULL,
-  `modelo` varchar(50) DEFAULT NULL,
-  `capacidad` int(11) DEFAULT NULL,
-  `empresa_rentadora` varchar(50) DEFAULT NULL,
-  `tipo` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_vehiculo`),
   KEY `id_producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 
 CREATE TABLE `detalle_carrito` (
   `id_detalle_carrito` int(11) NOT NULL,
   `id_carrito` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL DEFAULT 1,
-  `precio_carrito` decimal(10,2) NOT NULL,
-  `fecha_reserva` date DEFAULT NULL,
   PRIMARY KEY (`id_detalle_carrito`),
   KEY `id_carrito` (`id_carrito`),
   KEY `id_producto` (`id_producto`)
@@ -118,14 +81,9 @@ CREATE TABLE `detalle_paquete` (
   KEY `id_producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
 CREATE TABLE `pedidos` (
   `id_pedido` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
-  `fecha` date NOT NULL,
-  `precio_total` float(11,2) NOT NULL,
-  `metodo_pago` enum('Tarjeta_debito','Tarjeta_credito','Debito','Transferencia_bancaria') DEFAULT NULL,
-  `cantidad` int(11) NOT NULL,
   PRIMARY KEY (`id_pedido`),
   KEY `id_usuario` (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -134,8 +92,6 @@ CREATE TABLE `detalle_pedido` (
   `id_detalle_pedido` int(11) NOT NULL,
   `id_pedido` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL DEFAULT 1,
-  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id_detalle_pedido`),
   KEY `id_pedido` (`id_pedido`),
   KEY `id_producto` (`id_producto`)
@@ -174,17 +130,10 @@ CREATE TABLE `ventas` (
   KEY `ventas_ibfk_1` (`estado_facturacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
 CREATE TABLE `mails_automaticos` (
   `id_mail` int(11) NOT NULL,
-  `destinatario` varchar(255) NOT NULL,
-  `asunto` varchar(255) NOT NULL,
-  `mensaje` text NOT NULL,
-  `fecha_envio` datetime NOT NULL DEFAULT current_timestamp(),
-  `estado_envio` enum('pendiente','enviado','fallido') NOT NULL DEFAULT 'pendiente',
   PRIMARY KEY (`id_mail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 
 ALTER TABLE `carrito`
   ADD CONSTRAINT `carrito_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
@@ -233,4 +182,4 @@ ALTER TABLE `vehiculos`
 
 ALTER TABLE `ventas`
   ADD CONSTRAINT `fk_ventas_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos_aprobados` (`id_pedido`) ON DELETE CASCADE,
-  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`estado_facturacion`) REFERENCES
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`estado_facturacion`) REFERENCES `estado_facturacion` (`id_estado`);
