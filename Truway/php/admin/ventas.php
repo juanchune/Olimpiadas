bc860ca93c7bfa65be024980a5d449f094181df3
Maia-SancL <sanchezmaialen6@gmail.com>
2025-06-17 20:31:24 -0300

    Ultimo

--- Truway/php/admin/ventas.php
+++ Truway/php/admin/ventas.php
@@ -43,6 +43,22 @@ while ($venta = mysqli_fetch_assoc($res_pendientes)) {
                       WHERE v.estado_facturacion = $estado";
             $result = mysqli_query($conexion, $query);
         ?>
+
+        <div class="cont-filtros">
+            <form method="get" action="" class="form-filtros">
+            <select class="select-filtro" name="tipo">
+                <option value="">Seleccione una fecha</option>
+                </option>
+
+            </select>
+
+                <div class="barra-buscar">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" viewBox="0 0 24 24"><path class="icon" fill="currentColor" d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0s.41-1.08 0-1.49zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14"/></svg>
+                    <input type="search" name="buscar" class="input-buscar" placeholder="Nombre o email de usuario" value="<?= isset($_GET['buscar']) ? htmlspecialchars($_GET['buscar']) : '' ?>">
+                </div>
+            <button class="btn-filtrar" name="buscar" type="submit">Buscar</button>
+            </form>
+        </div>
         
          <section class="section-tabla-productos">
                 <article class="producto">
