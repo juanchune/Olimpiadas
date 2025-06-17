2a8173fd2ae442464b2ea7cc3c6c2a3aff269a1c
Jolareka <68790439+jolareka@users.noreply.github.com>
2025-06-17 20:33:27 -0300

    Update truway.sql

--- Truway/db/truway.sql
+++ Truway/db/truway.sql
@@ -214,6 +214,8 @@ CREATE TABLE `mails_automaticos` (
   PRIMARY KEY (`id_mail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE productos
+ADD COLUMN codigo_producto VARCHAR(12);
 
 INSERT INTO `usuarios` (`nombre`, `apellido`, `email`, `contrasena`, `fecha_nacimiento`, `telefono`, `rol`) VALUES
 ('Joaquin', 'Roldan', 'roldanjoaquind42@gmail.com', 'b4b147bc522828731f1a016bfa72c073', '2006-09-26', '+542262540188', 'cliente'),
